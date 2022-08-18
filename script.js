a1e08259e44bc16363f78d745dcdf4c6c00e7f3a
Andrey <andrey.berenshtein@yandex.ru>
2022-08-18 20:18:32 +0300

    add new modal window

--- script.js
+++ script.js
@@ -6,3 +6,7 @@ alert(`Тебя зовут ${UserName}`);
 
 let age = prompt(`Сколько тебе лет?`, 18);
 alert(`Тебе ${age} лет!`);
+
+let sity = prompt(`Из какого ты города?`, 'Sity');
+const result = confirm(`${UserName} ты из ${sity} и тебе ${age} лет?`);
+console.log(result);
